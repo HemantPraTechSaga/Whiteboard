e6834cff1f5fa91ecade0dc9b6e7710edc8f7f96
HemantPraTechSaga <hemantpra.techsaga@gmail.com>
2025-04-30 14:48:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const WebSocket = require('ws');
 const express = require('express');
 const app = express();
 
+// Create WebSocket server
 const server = new WebSocket.WebSocketServer({ port: 8080 });
 
 server.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.on('message', (message) => {
-        console.log(message.toString());
+        console.log('Received message:', message.toString());
 
         let data;
         try {
@@ -17,19 +18,45 @@ server.on('connection', (ws) => {
             console.error('Invalid JSON received:', message);
             return;
         }
-        // if (data.type === 'draw') {
-        // Broadcast drawing data to all connected clients
-        server.clients.forEach((client) => {
-            // if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(data));
-            // }
-        });
-        // }
+
+        // Handle different message types
+        if (data.type === 'draw') {
+            // Broadcast drawing data to all connected clients
+            server.clients.forEach((client) => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data));
+                }
+            });
+        }
+
+        // Respond to "hit" messages
+        if (data.type === 'hit') {
+            const response = { type: 'response', message: 'Hit received!' };
+            ws.send(JSON.stringify(response));  // Send response back to the client
+        }
     });
 
     ws.on('close', () => {
         console.log('Client disconnected');
     });
+});
+
+// Serve a simple Express endpoint for testing the server
+app.get('/', (req, res) => {
+    res.send('WebSocket server is running');
+});
+
+// Test endpoint to return a sample response
+app.get('/test', (req, res) => {
+    res.json({
+        status: 'success',
+        message: 'This is a test response!',
+    });
+});
+
+// Start Express server
+app.listen(3000, () => {
+    console.log('Express server running on http://localhost:3000');
 });
 
 console.log('WebSocket server is running on ws://localhost:8080');
